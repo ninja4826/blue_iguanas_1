210edcd4df004faf919f0aa17de8c5cbf0a75b24
Russell Hueske <hueske.russ690@gmail.com>
2016-07-17 13:31:15 -0500

    Fixed some stuff up with reset scripts

--- reset.bat
+++ reset.bat
@@ -3,8 +3,7 @@ setlocal EnableDelayedExpansion
 setlocal EnableExtensions
 
 del db.sqlite3
-set python=/usr/bin/python
-!python! ./manage.py migrate
+python ./manage.py migrate
 if exist ./todo/fixtures/seed.json (
-  !python! ./manage.py loaddata seed
+  python ./manage.py loaddata seed
 )
